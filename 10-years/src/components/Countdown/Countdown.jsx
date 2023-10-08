bc83d457a13e975ddf21f22040be92f60ccf3a01
silverirac <caueasilveira@gmail.com>
2023-10-08 19:47:23 -0300

    build

--- 10-years/src/components/Countdown/Countdown.jsx
+++ 10-years/src/components/Countdown/Countdown.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { CountdownPlate } from "./CountDownPlate.jsx";
+import { CountdownPlate } from "./CountDo0wnPlate.jsx";
 import { useNavigate } from "react-router-dom";
 
 export const Countdown = ({ expectedDate, hasYears, hasMonths, hasDays, hasHours, hasMinutes, hasSeconds}) => {
